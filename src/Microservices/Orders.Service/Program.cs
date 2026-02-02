50d30766f2f81c37d000d36e69722b7fe641996d
JuanDavid_Ospina <juandavid.ospina@dellteam.com>
2026-02-02 10:37:32 -0500

    * the .net framwork was downgrade to 8 * the orders.services is ready to test

--- src/Microservices/Orders.Service/Program.cs
+++ src/Microservices/Orders.Service/Program.cs
@@ -1,4 +1,6 @@
+using System;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Orders.Service.Application.Orders.CreateOrder;
 using Orders.Service.Application.Orders.GetOrder;
 using Orders.Service.Infrastructure.Persistence;
@@ -6,23 +8,59 @@ using Orders.Service.Infrastructure.Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 
-builder.Services.AddDbContext<OrdersDbContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Mi API",
+        Version = "v1",
+        Description = "Web API en .NET 8 con Swagger"
+    });
+});
+
+//builder.Services.AddDbContext<OrdersDbContext>(opt =>
+//    opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddDbContext<OrdersDbContext>(options =>
+{
+    options.UseInMemoryDatabase("TransportDB");
+});
 
 builder.Services.AddScoped<CreateOrderHandler>();
 builder.Services.AddScoped<GetOrderHandler>();
 
 var app = builder.Build();
 
+// Seed de datos (opcional pero recomendado)
+//using (var scope = app.Services.CreateScope())
+//{
+//    var db = scope.ServiceProvider.GetRequiredService<OrdersDbContext>();
+
+//    if (!db.Productos.Any())
+//    {
+//        db.Productos.AddRange(
+//            new Producto { Id = 1, Nombre = "Teclado", Precio = 100 },
+//            new Producto { Id = 2, Nombre = "Mouse", Precio = 50 }
+//        );
+
+//        db.SaveChanges();
+//    }
+//}
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mi API v1");
+    });
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 app.Run();
 
